6c9e12de4f1ed995e5cefe03930f4e06c30a24bf
Joaquin Rodriguez <jrodriguezblangino@gmail.com>
2025-06-12 06:55:02 +0200

    Creacion del schema de la base de datos

---
+++ sql/schema.sql
@@ -0,0 +1,6 @@
+-- ============================================================================
+-- Proyecto “Car-Sharing Fleet Analysis”
+-- Objetivo: Mostrar de forma progresiva y práctica mis conocimientos en MySQL
+-- Módulo/Día 1: Definición del esquema de la base de datos
+-- ============================================================================
+
