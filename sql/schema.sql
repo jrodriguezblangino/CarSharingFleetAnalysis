2804b8f7c0a30039f78521783c50980fbcbda01e
Joaquin Rodriguez <jrodriguezblangino@gmail.com>
2025-06-12 06:59:56 +0200

    Creacion de la primera tabla

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,3 +4,11 @@
 -- Módulo/Día 1: Definición del esquema de la base de datos
 -- ============================================================================
 
+-- 1) Tabla de estaciones
+CREATE TABLE stations (
+    station_id   INT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(100) NOT NULL,
+    address      VARCHAR(255) NOT NULL,
+    capacity     INT         NOT NULL,       
+    created_at   DATETIME    DEFAULT NOW()
+);
