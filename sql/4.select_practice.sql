5bcb8fb2e5ee7783f8c59fab917285a9db10d33c
Joaquin Rodriguez <jrodriguezblangino@gmail.com>
2025-06-15 21:56:22 +0200

    Consultas intermedias

--- sql/4.select_practice.sql
+++ sql/4.select_practice.sql
@@ -21,4 +21,35 @@ SELECT * FROM reservations ORDER BY start_time DESC LIMIT 10;
 SELECT * FROM maintenance WHERE end_date IS NULL;
 
 -- 5. Contar cuántos vehículos hay por estado
-SELECT status, COUNT(*) AS cantidad FROM vehicles GROUP BY status;
+SELECT status, COUNT(*) AS cantidad FROM vehicles GROUP BY status;3
+
+-- ===============================
+-- Nivel 2: Consultas Intermedias
+-- ===============================
+
+-- 6 Cantidad de vehículos hay en cada estación
+SELECT s.name AS estacion, COUNT(v.id) AS cantidad_vehiculos
+FROM stations s
+LEFT JOIN vehicles v ON s.id = v.station_id
+GROUP BY s.name;
+
+-- 7. Listar todos los vehículos que hayan tenido alguna reserva
+SELECT DISTINCT v.license_plate, v.model
+FROM vehicles v
+JOIN reservations r ON v.id = r.vehicle_id;
+
+-- 8. Ver reservas que duran más de 2 horas
+SELECT *, 
+       TIMESTAMPDIFF(MINUTE, start_time, end_time) AS duracion_minutos
+FROM reservations
+WHERE TIMESTAMPDIFF(MINUTE, start_time, end_time) > 120;
+
+-- 9. Mostrar todos los vehículos que actualmente están en uso o reservados
+SELECT * FROM vehicles WHERE status IN ('in_use', 'reserved');
+
+-- 10. Cantidad total de reservas por vehículo
+SELECT v.license_plate, COUNT(r.id) AS total_reservas
+FROM vehicles v
+LEFT JOIN reservations r ON v.id = r.vehicle_id
+GROUP BY v.license_plate
+ORDER BY total_reservas DESC;
